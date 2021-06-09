8eaf8bda7d28d3fb3879c33d80530e82a46e287b
JongSoo Hwang <jongsoo.data@gmail.com>
2021-06-09 23:54:14 +0900

    입국심사 문제 풀이 작성
    입국심사 문제 풀이 작성

--- algorithm/programmers/immigration-question.py
+++ algorithm/programmers/immigration-question.py
@@ -17,3 +17,35 @@ def solution(n, times):
     return answer 
   
   ## 시간 초과 실패
+
+# 제출 답안 (2021-06-09)
+# 이분탐색 적용
+
+def solution(n, times):    
+    
+    left = 1
+    right = min(times) * n 
+    mid = (left + right)/2
+    
+    def cal_n(t):
+        return sum([t//i for i in times])
+    
+    while True:
+        if cal_n(mid) >= n :
+            right = mid - 1
+            if cal_n(right) == n-1:
+                return int(right) + 1
+                
+            mid = (left + right)//2
+            
+        if cal_n(mid) < n :
+            left = mid + 1
+            if cal_n(left) >= n:
+                return int(left)
+            
+            mid = (left + right)//2
+            
+        if right ==left:
+            return right
+
+        ## 정답 처리 됨
